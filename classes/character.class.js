e17f6199e0158eb3c340553692b85c23d90fb8c7
Franziska Kalloch <franzy@MacBook-Air-von-Franziska.fritz.box>
2026-04-21 19:16:47 +0200

    feat: add character walking animation array

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,4 +1,14 @@
 class Character extends MoveAbleObject {
+  imageCache = [];
+  imageWalks = [
+    'img/2_character_pepe/2_walk/W-21.png',
+    'img/2_character_pepe/2_walk/W-22.png',
+    'img/2_character_pepe/2_walk/W-23.png',
+    'img/2_character_pepe/2_walk/W-24.png',
+    'img/2_character_pepe/2_walk/W-25.png',
+    'img/2_character_pepe/2_walk/W-26.png',
+  ];
+
   constructor() {
     super();
     this.loadImage('img/2_character_pepe/2_walk/W-21.png');
--- classes/moveAble-object.class.js
+++ classes/moveAble-object.class.js
@@ -4,13 +4,13 @@ class MoveAbleObject {
   img;
   height = 300;
   width = 150;
+  imageCache = []; // ein Speicher von Bildern
 
   // loadImage('img/test.png')
   loadImage(path) {
     this.img = new Image(); // ist das gleich wie, this.img = document.getElementById('image');
     this.img.src = path;
   }
-
   moveRight() {
     console.log('moving right');
   }
